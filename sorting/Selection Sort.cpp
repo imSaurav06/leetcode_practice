fa8151525a18ac2ba728af7d10635f6c0f530736
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-14 13:43:19 +0000

    up

---
+++ sorting/Selection Sort.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[] ,int n){
+
+    for(int i=0; i<n; i++){
+        int min=i;
+        for(int j=i+1; j<n; j++){
+            
+            if(arr[j]<arr[min]){
+
+                min = j;
+                
+            };
+            
+        };
+        swap(arr[i],arr[min]);
+    };
+};
+

fc2ff8346af19e4a6f15ad7c20566b3e59bad8f9
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-15 20:35:06 +0000

    sorting

--- sorting/Insertion Sorting.cpp
+++ sorting/Insertion Sorting.cpp
@@ -1,18 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void BubbleSort(vector<int>arr, int n) {
+void InsertionSort(vector<int>arr, int n) {
 
-    for(int i=n-1; i>0; i--){
-        for(int j=0; j<i; j++){
-            if(arr[j]<arr[j+1])  //descending 
-           //  if(arr[j]>arr[j+1])  //ascending 
-            {
-                swap(arr[j],arr[j+1]);
-            }
-        }
-        
-    };
+   
   for(auto x : arr ){
         cout<<"-"<<x;
     };

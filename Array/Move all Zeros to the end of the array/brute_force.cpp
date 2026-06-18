e1a7da379adbf810d414d6fb2111b8843e0219f4
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-18 21:50:30 +0000

    .

---
+++ Array/Move all Zeros to the end of the array/brute_force.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> movezero(vector<int>&arr){
+   
+}
+
+int main(){
+
+    vector<int>m;
+    m.push_back(1);
+    m.push_back(2);
+    m.push_back(3);
+   
+   
+    
+
+
+for(auto x : m){
+    cout<<x<<' ';
+}
+
+cout<<'\n';
+
+movezero(m);
+
+for(auto x : m){
+    cout<<x<<' ';
+}
+
+ return 0;
+}

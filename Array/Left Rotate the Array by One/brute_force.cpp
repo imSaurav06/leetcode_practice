da8a0cef63c107fb3b62dab084e7c84019fb7f65
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-18 21:18:37 +0000

    .

---
+++ Array/Left Rotate the Array by One/brute_force.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> rotatebyone(vector<int>&m){
+
+}
+
+int main(){
+
+    vector<int>m;
+    m.push_back(12);
+    m.push_back(34);
+    m.push_back(34);
+    m.push_back(34);
+    m.push_back(54);
+    m.push_back(23);
+    m.push_back(23);
+    m.push_back(23);
+    m.push_back(1);
+    m.push_back(1);
+    m.push_back(2);
+    m.push_back(2);
+
+
+for(auto x : m){
+    cout<<x<<' ';
+}
+
+cout<<'\n';
+
+rotatebyone(m);
+
+ return 0;
+}

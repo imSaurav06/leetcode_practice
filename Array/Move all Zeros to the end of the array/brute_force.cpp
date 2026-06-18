b2f525a4b0c51394ff0f340aa64179d9b5e56100
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-18 22:17:37 +0000

    .

--- Array/Move all Zeros to the end of the array/brute_force.cpp
+++ Array/Move all Zeros to the end of the array/brute_force.cpp
@@ -3,15 +3,39 @@
 using namespace std;
 
 vector<int> movezero(vector<int>&arr){
+    vector<int> temp;
+    int n = arr.size();
+
+     for(int i=0; i<n; i++){
+
+        if(arr[i] != 0){ temp.push_back(arr[i]);}
+        
+
+     };
+      for(int j=0; j<n; j++){
+        
+        if(arr[j] == 0) {temp.push_back(arr[j]);}
+       
+
+     }
+
+
+    return temp;
+
    
-}
+};
 
 int main(){
 
     vector<int>m;
     m.push_back(1);
+    m.push_back(0);
+    m.push_back(0);
     m.push_back(2);
     m.push_back(3);
+    m.push_back(0);
+    m.push_back(0);
+    m.push_back(4);
    
    
     
@@ -23,9 +47,9 @@ for(auto x : m){
 
 cout<<'\n';
 
-movezero(m);
+vector<int> result = movezero(m);
 
-for(auto x : m){
+  for(auto x : result){
     cout<<x<<' ';
 }
 

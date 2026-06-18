70fcab81df3417d87930fdf32f932ad77ba4c127
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-18 19:48:08 +0000

    .

---
+++ Array/Check if an Array is Sorted/brute_force.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>vec;
+    int q = 6;
+   while(q--){
+    int n;
+    cin >> n;
+    vec.push_back(n);
+   }
+
+   bool result = ShortOrnot(vec)
+}
